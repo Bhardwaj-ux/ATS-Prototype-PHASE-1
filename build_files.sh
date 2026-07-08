86d964d6289f256016dbcc8a5b483bf999e58611
Himanshu Bhardwaj <himanshubhardwaj@Himanshus-MacBook.local>
2026-07-08 14:58:02 +0530

    Install requirements into .vercel_build before running migrations

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 set -o errexit
 
+# Install Python dependencies into a local directory that will be uploaded
+# with the build so the ASGI function has access to them during runtime.
+BUILD_LIB_DIR=".vercel_build_python"
+mkdir -p "$BUILD_LIB_DIR"
+python3 -m pip install --upgrade pip || true
+python3 -m pip install --no-cache-dir -r requirements.txt -t "$BUILD_LIB_DIR"
+
+# Ensure the build lib is on PYTHONPATH when running manage commands
+export PYTHONPATH="$PWD/$BUILD_LIB_DIR:$PYTHONPATH"
+
 python manage.py migrate --noinput
